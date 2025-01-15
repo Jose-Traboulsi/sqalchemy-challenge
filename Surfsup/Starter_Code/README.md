8fe66e3fa9ced045dc0c11d7b4e2839f4c02c64a
Jose_Traboulsi <jose.traboulsiricha@alumni.utoronto.ca>
2025-01-14 19:51:29 -0500

    Uploading work on this Challenge

--- Surfsup/Starter_Code/README.md
+++ Surfsup/Starter_Code/README.md
@@ -1,1 +1,22 @@
 # sqalchemy-challenge
+
+I referred to class materials and used AI when needed to fix typos or errors on query structure. I also booked tutoring sessions to go over the class materials to enhace my
+understanding of it.
+
+Exploratory Precipitation Analysis
+The key here is to obtain the last date in the data so we can calculate the starting date for the analysis by using the datetime library.
+We make a query for the date table, organize it in descending order, and then use the .first() function to get the first row of data (i.e. the most recent date).
+We then use timedelta to find the start date. Months are not an allowed parameter within the datetime library's timedelta() fuction, so the key here was to use 365 days.
+We sort the dates in descending order so we can plot the values, since our x axis are the dates.
+I used the .plot function directly in the sorted data_frame as the output was clearner (i.e., when I tried using .plt method, the legend on the graph was not readable since it
+showed all dates in the series vs. a few sample ones, like the version provided in the solution).
+To obtain the summary statistics, we use the .describe() function. For visual appeal, I converted the results into a data frame.
+
+Exploratoy Station Analysis
+To find out the total number of stations, we make a query on station class using for station names and the total row count in this column.
+For the latter, we use .func.count. I added .scalar() so we can extract the result, but it was not needed to find the solution.
+To rank stations by number of observations, the trick is to use group by on a query of station names and the count of measurements so we can get the total count per station. We add .desc() to specificy the results should be on descending order by observation count. From the results we can take the "most active" station id ('USC00519281').
+With the code in hand, we can proceed to calculate the max, min and avg. temperatures at this station.
+Next, we can use this id to filter the results on a query of measurement dates combined with the .max() function to extract the most recent observation date for this station.
+With this date, we can then proceed to obtain results from there onwards that we can use in the histogram. I have added some comments on my Jupyter Notebook to explain some of the logic behind my code.
+
--- Surfsup/Starter_Code/app.py
+++ Surfsup/Starter_Code/app.py
@@ -1,30 +1,120 @@
 # Import the dependencies.
-
-
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+station = Base.classes.station
+measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start_date (format: YYYY-MM-DD, e.g., /api/v1.0/2025-01-14)<br/>"
+        f"/api/v1.0/start_date/end_date (format: YYYY-MM-DD, e.g., /api/v1.0/2025-01-14/2025-12-31)"
+    )
+
+@app.route("/api/v1.0/precipitation")
+
+def prcp_func():
+    end_date = dt.date(2017, 8, 23)
+    start_date = dt.date(2016, 8, 23)
+
+    results = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date >= start_date).\
+        filter(measurement.date <= end_date).all()
+    
+    session.close()
+
+
+    # Create a dictionary with date as the key and prcp as the value
+    prcp_dict = {date: prcp for date, prcp in results}
+
+    return jsonify(prcp_dict)
+
+
+@app.route("/api/v1.0/stations")
+def station_func():
+    results2 = session.query(station.station).all()
+    all_stations = list(np.ravel(results2))
+
+    session.close()
+
+    return jsonify(all_stations)
+
+
+@app.route("/api/v1.0/tobs")
+def active_station():
+    start_date = dt.date(2016, 8, 18)
+    tobs_data = session.query(measurement.date, measurement.tobs).\
+        filter(measurement.station=='USC00519281').\
+        filter(measurement.date >= start_date).all()
+
+    session.close()
+
+    active_station = list(np.ravel(tobs_data))
+    return jsonify(active_station)
+
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+
+def date_func(start=None, end=None):
+    select=[func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
+    if not end:
+        start = dt.datetime.strptime(start, "%Y-%m-%d")
+        results = session.query(*select).\
+            filter(measurement.date >= start).all()
+            
+        session.close()
+
+        date_results=list(np.ravel(results))
+        return jsonify(date_results)
+
+    start = dt.datetime.strptime(start, "%Y-%m-%d")
+    end = dt.datetime.strptime(end, "%Y-%m-%d")
+    results = session.query(*select).\
+         filter(measurement.date >= start).\
+         filter(measurement.date <= end).all()
+
+    session.close()    
+
+    date_results2=list(np.ravel(results))
+    return jsonify(date_results2)
+
+if __name__ == "__main__":
+    app.run(debug=True)
